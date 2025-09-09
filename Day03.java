3791dda5b43bd3873d3186c1c2c832a14a5ae3be
Aldo Saputra / Informatika <aldopadaungan@gmail.com>
2025-09-10 07:30:39 +0800

    Day03.java

---
+++ Day03.java
@@ -0,0 +1,15 @@
+public class Comentar {
+    public static void main(String[] args) {
+            
+        // Komentar ini di gunakan untuk 1 baris saja
+        
+        /* Komentar ini di gunakan untuk banyak baris seperti ini:
+            -  
+            -
+            -
+            -
+            -*/
+        
+    }
+    
+}
