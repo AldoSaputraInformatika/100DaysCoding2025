e42cc0e85420f5cdb209a86ae6587b29dbf4c076
Aldo Saputra / Informatika <aldopadaungan@gmail.com>
2025-09-13 06:31:41 +0800

    Day06.java

---
+++ Day06.java
@@ -0,0 +1,12 @@
+public class Tipedata{
+    public static void main(String[]args){
+        //Tipe data pecahan
+       
+        double A = 90.5;
+        // ukuran 64 bit (8 byte)
+        float B = (float) 90.8;
+        // ukuran 32 biit (4 byte)
+        System.out.println(A);
+        System.out.println(B);
+    }
+}
