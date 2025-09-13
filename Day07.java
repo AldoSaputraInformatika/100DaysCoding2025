1ff52b442213e5270f6f13493bb2017b3b814a2f
Aldo Saputra / Informatika <aldopadaungan@gmail.com>
2025-09-14 07:00:00 +0800

    Day07.java

---
+++ Day07.java
@@ -0,0 +1,15 @@
+public class Tipedata{
+    public static void main(String[]args){
+       // char tipe data untuk satu karakter saja dan memakai petik satu
+       char nilai_ujian = 'A';
+       // boolean tipe data yang hanya punya 2 nilai (True dan False)
+       boolean a = false;
+       boolean b = true;
+       
+        System.out.println(nilai_ujian);
+        System.out.println(a);
+        System.out.println(b);
+      
+    }
+    
+}
