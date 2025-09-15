38e9b2e9a56ee777572fbbb5e26adf0f1074f7ac
Aldo Saputra / Informatika <aldopadaungan@gmail.com>
2025-09-15 12:37:11 +0800

    Day08.java

---
+++ Day08.java
@@ -0,0 +1,20 @@
+public class Tipedatakaarakter {
+    public static void main(String[] args) {
+       
+        // String merupakan tipe data bukan primitif
+        // String di apit tanda kutip sebelum dan sesudah text
+        // String bernilai teks bukan angka
+        
+        String nama = "Aldo";
+        String kelas = "C";
+        String fakultas = "Teknik";
+        String prodi= "Informatika";
+        
+        System.out.println("Nama: "+ nama);
+        System.out.println("Kelas: "+ kelas);
+        System.out.println("Fakultas: "+ fakultas);
+        System.out.println("Prodi: "+ prodi);
+  
+    }
+    
+}
