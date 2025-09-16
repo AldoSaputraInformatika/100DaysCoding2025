6bf343646c956c2404445014f73e90e4308559ec
Aldo Saputra / Informatika <aldopadaungan@gmail.com>
2025-09-17 07:40:33 +0800

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,16 @@
+package finalvariable;
+public class FinalVariable {
+    public static void main(String[] args) {
+        final var nama = "aldo";
+        final var umur = 19;
+        final double berat = 75.9;
+        final char nilai = 'A';
+        /*Mengunnci nilai suatu variable*/
+        System.out.println("Nama: "+ nama);
+        System.out.println("Umur: "+ umur);
+        System.out.println("Berat: "+ berat);
+        System.out.println("Nilai: "+ nilai);
+        
+    }
+    
+}
