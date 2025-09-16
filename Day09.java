f5d1f7bfbbfb83da21de5078be02a31e5988f51c
Aldo Saputra / Informatika <aldopadaungan@gmail.com>
2025-09-16 11:03:22 +0800

    Create Day09.java

---
+++ Day09.java
@@ -0,0 +1,22 @@
+package deklarasiinisialisaliupdatevariabel;
+public class DeklarasiInisialisaliUpdateVariabel {
+    public static void main(String[] args) {
+ 
+      int umur = 50;
+      /* Saya memdeklarasikan suatu variabel bernama umur 
+      bertipe integer dan menginisialisasikan atau memberi 
+      nilai awal variabel tersebut 50*/
+     
+     umur += 1;
+     umur += 3;
+     umur *= 3;
+     /* saya mengupdate/memperbarui nilai variabelnya sebanyak 3x 
+     dengan nilai awal 50 ke 51 ke 54 ke 162 dimana 162
+     adalah nilai akhir dari variabel umur
+     */
+     
+     System.out.println(umur);
+
+    }
+    
+}
