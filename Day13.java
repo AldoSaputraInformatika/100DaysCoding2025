faa9a4694b1e07662895ccc80a9a84f25f73b57b
Aldo Saputra / Informatika <aldopadaungan@gmail.com>
2025-09-20 16:07:35 +0800

    Day13.java

--- Day13.java
+++ Day13.java
@@ -1,19 +1,26 @@
-package evaluasimentor3;
-public class EvaluasiMentorPart3 {
+package evaluasimentor5;
+
+import java.util.Scanner;
+
+public class EvaluasiMentor5 {
     public static void main(String[] args) {
-        String nama;
-        String kelas;
-        String status;
-        
-        nama = "\tAldo Saputra";
-        kelas = "\tKelas C";
-        status = "\t\"Aktif\"";
-        
-        System.out.print("Boidata Mahasiswa \n-----------------\n");
-        System.out.println("NAMA:"+nama);
-        System.out.println("KELAS:"+kelas);
-        System.out.println("STATUS:"+status);
-                
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukkan nama:"  );
+        String nama = input.nextLine();
+        System.out.println("Masukkan Nim:");
+        final String Nim = input.nextLine();
+        System.out.println("Masukkan umur:");
+        int umur = input.nextInt();
+        System.out.println("Masukkan tinggi:");
+        double tinggi = input.nextDouble();
+        System.out.println("Masukkan JK:");
+        char Jk = input.next().charAt(0);
+        System.out.print("BIODATA\n--------\n");
+        System.out.println("Nama: "+ nama);
+        System.out.println("Nim: "+ Nim);
+        System.out.println("Umur: "+ umur);
+        System.out.println("Tinggi:"+ tinggi+ "CM");
+        System.out.println("Jenis Kelamin "+ Jk);
     }
     
 }
