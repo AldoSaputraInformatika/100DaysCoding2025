da6216ba9d0459df3cf3b2712d90e84f5ac9a81c
Aldo Saputra / Informatika <aldopadaungan@gmail.com>
2025-09-20 16:05:09 +0800

    Day13.java

---
+++ Day13.java
@@ -0,0 +1,19 @@
+package evaluasimentor3;
+public class EvaluasiMentorPart3 {
+    public static void main(String[] args) {
+        String nama;
+        String kelas;
+        String status;
+        
+        nama = "\tAldo Saputra";
+        kelas = "\tKelas C";
+        status = "\t\"Aktif\"";
+        
+        System.out.print("Boidata Mahasiswa \n-----------------\n");
+        System.out.println("NAMA:"+nama);
+        System.out.println("KELAS:"+kelas);
+        System.out.println("STATUS:"+status);
+                
+    }
+    
+}
