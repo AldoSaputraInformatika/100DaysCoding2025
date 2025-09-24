12cf5ea77f7615bd6a5c495a40fabebd364a5ca2
Aldo Saputra / Informatika <aldopadaungan@gmail.com>
2025-09-24 19:36:39 +0800

    Day17.java

--- Day17.java
+++ Day17.java
@@ -3,8 +3,11 @@ import java .util.Scanner;
 public class Day17 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan Angka pertama: ");
         int angka = input.nextInt();
+        System.out.print("Masukkan Angka kedua: ");
         int angka2 = input.nextInt();
+        // % merupakan operator aritmatika untuk mencari sisa bagi dari suatu operasiii
         int hasil_bagi = angka % angka2;
         System.out.println("HASIL BAGI DARI " + angka + " dan " + angka2 + " adalah " + hasil_bagi);
     }
