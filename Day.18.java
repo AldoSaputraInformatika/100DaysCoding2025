81fee547d740db78d9cf36bbe931b151d437fcd5
Aldo Saputra / Informatika <aldopadaungan@gmail.com>
2025-09-25 21:45:47 +0800

    Day.18.java

---
+++ Day.18.java
@@ -0,0 +1,11 @@
+package konversitipedata;
+
+public class KonversiTipeData {
+
+    public static void main(String[] args) {
+        byte A = 127;
+        double B = A;
+        System.out.println(B);
+    
+    }
+}
