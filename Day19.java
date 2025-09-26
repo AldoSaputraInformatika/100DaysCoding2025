e4032bf3b74d5eb940d5dbe92773a48ced86b0b8
Aldo Saputra / Informatika <aldopadaungan@gmail.com>
2025-09-26 21:36:49 +0800

    Day19.java

---
+++ Day19.java
@@ -0,0 +1,9 @@
+package day19;
+public class Day19 {
+    public static void main(String[] args) {
+        double A = 9.89;
+        int B = (int) A;
+        System.out.println("Hasil convert " +  B);
+        
+    }
+}
