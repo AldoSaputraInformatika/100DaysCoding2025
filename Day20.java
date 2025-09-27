cb652098cba9a147f9efcbb379a8334913eb51ac
Aldo Saputra / Informatika <aldopadaungan@gmail.com>
2025-09-27 20:15:10 +0800

    Day20.java

---
+++ Day20.java
@@ -0,0 +1,11 @@
+package day20;
+public class Day20 {
+    public static void main(String[] args) {
+        long angka = 127;
+        String angka2 = String.valueOf(angka);
+        System.out.println(angka2);
+        
+        String angka3 = String.valueOf(-127);
+        System.out.println(angka3);
+    }
+}
