7cfb11b8f3cfa7c103d60c7380e1d1e0666baeba
Aldo Saputra / Informatika <aldopadaungan@gmail.com>
2025-09-28 20:48:25 +0800

    Day21.java

---
+++ Day21.java
@@ -0,0 +1,18 @@
+package day21;
+public class Day21 {
+    public static void main(String[] args) {
+    int A = 8;
+    int B = 4;
+    
+    A = A + B;
+    B = A - B;
+    A = A - B;
+    
+    
+        System.out.println("A = " + A);
+        System.out.println("B = " + B);
+   
+    
+    }
+    
+}
