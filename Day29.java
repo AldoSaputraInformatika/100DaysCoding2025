bdce46cc3da0061cf72eed605252c8b4bf950cec
Aldo Saputra / Informatika <aldopadaungan@gmail.com>
2025-10-06 21:07:12 +0800

    Day29.java

---
+++ Day29.java
@@ -0,0 +1,14 @@
+package day29;
+
+public class Day29 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        
+        System.out.println(a < b);
+        System.out.println(a > b);
+        System.out.println("=====");
+        System.out.println(b > a);
+        System.out.println(b < a);
+    }
+}
