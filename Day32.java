14d8e08a9a3a04ee4cff34a16052ed66fa9be08a
Aldo Saputra / Informatika <aldopadaungan@gmail.com>
2025-10-09 21:59:27 +0800

    Day32.java

---
+++ Day32.java
@@ -0,0 +1,14 @@
+package day32;
+
+import java.util.Scanner;
+
+public class Day32 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean a = input.nextBoolean();
+        boolean b = input.nextBoolean();
+        
+        System.out.println(a || b);
+    }
+    
+}
