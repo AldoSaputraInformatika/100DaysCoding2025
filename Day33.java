b360cd113d18c364c275c2b4d3d6cd233a182bc5
Aldo Saputra / Informatika <aldopadaungan@gmail.com>
2025-10-10 20:16:37 +0800

    Day33.java

---
+++ Day33.java
@@ -0,0 +1,9 @@
+package day33;
+public class Day33 {
+    public static void main(String[] args) {
+       boolean a = true;
+       //Not(!) dalam java memiliki arti yaitu membalik nilai
+       System.out.println(!a);
+    }
+    
+}
