2f6c419d5af8b0086457570ffda721087df11bff
Aldo Saputra / Informatika <aldopadaungan@gmail.com>
2025-10-12 22:02:13 +0800

    Day35.java

---
+++ Day35.java
@@ -0,0 +1,20 @@
+package day35;
+
+import java.util.Scanner;
+
+public class Day35 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int angka = input.nextInt();
+        if (angka == 1) {
+            System.out.println("Keren");
+        } else if (angka == 2) {
+            System.out.println("Mantap");
+        } else if (angka == 3) {
+            System.out.println("Hebat");
+        } else {
+            System.out.println("Mantap");
+        }
+    }
+}
