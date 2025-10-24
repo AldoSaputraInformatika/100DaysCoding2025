09b16264c4259264b88ce6e279b02ed81fa5ac51
Aldo Saputra / Informatika <aldopadaungan@gmail.com>
2025-10-24 21:30:14 +0800

    Day47.java

---
+++ Day47.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Day47 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan angka: ");
+        int angka = input.nextInt();
+        String hari = null;
+        switch (angka) {
+            case 1:
+                hari = "Senin";
+                break;
+            case 2:
+                hari = "Selasa";
+                break;
+            case 3:
+                hari = "Rabu";
+                break;
+            case 4:
+                hari = "Kamis";
+                break;
+            case 5:
+                hari = "Jumat";
+                break;
+            case 6:
+                hari = "Sabtu";
+                break;
+            case 7:
+                hari = "Minggu";
+                break;
+            default:
+                hari = "Angka tidak valid";
+        }
+        if (angka >= 1 && angka <= 7) {
+            System.out.println("Hari ini adalah " + hari);
+        } else {
+            System.out.println(hari);
+        }
+    }
+}
