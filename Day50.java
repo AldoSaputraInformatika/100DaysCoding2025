e417b97395707e4977a779aba668de320856cb0d
Aldo Saputra / Informatika <aldopadaungan@gmail.com>
2025-10-27 21:26:08 +0800

    Day50.java

---
+++ Day50.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class Day50 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan angka: ");
+        int angka = input.nextInt();
+        String angka2 = angka % 2 == 0 ? "Genap" : "Ganjil";
+        System.out.println(angka + " " + angka2);
+
+    }
+
+}
