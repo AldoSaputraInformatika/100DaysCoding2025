b3d8f89b68a4a63a60886c737b96f9af64e77554
Aldo Saputra / Informatika <aldopadaungan@gmail.com>
2025-11-04 21:46:29 +0800

    Day58.java

---
+++ Day58.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Day58 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int N = input.nextInt();
+        for (int i = 1; i <= N; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = N; i >= 1; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
