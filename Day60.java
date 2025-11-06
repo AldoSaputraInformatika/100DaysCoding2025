02a8d7b7768ed751b7c038ade86b8664ebdd955e
Aldo Saputra / Informatika <aldopadaungan@gmail.com>
2025-11-06 20:19:59 +0800

    Day6

--- Day60.java
+++ Day60.java
@@ -1,10 +1,14 @@
-Scanner input = new Scanner(System.in);
+
+import java.util.Scanner;
+
+public class Day60 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         int N = input.nextInt();
         for (int i = N; i > 0; i--) {
             if(i % 2 == 0){
                 System.out.print(i +" ");
-            }
-            
+            }    
         }
         System.out.println();
         for (int i = N; i > 0; i--) {
@@ -12,3 +16,6 @@ Scanner input = new Scanner(System.in);
                 System.out.print(i + " ");
             }
         }
+        System.out.println();
+    }
+}
