70fb4092b83ef55d67c30924579e76b2a32f0e4a
Aldo Saputra / Informatika <aldopadaungan@gmail.com>
2025-11-12 21:22:05 +0800

    Day66.java

---
+++ Day66.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Day66dua {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        for (int i = 1; i <= n; i++) {
+            System.out.println("H");
+            System.out.println("A");
+            System.out.println("L");
+            System.out.println("O");
+            System.out.println();
+        }
+    }
+
+}
