adef82609204fb291b6937d1033f60e56d7293aa
Aldo Saputra / Informatika <aldopadaungan@gmail.com>
2025-11-13 21:02:37 +0800

    Day67.java

---
+++ Day67.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class Day67 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        for (int i = 1; i <= n; i++) {
+            System.out.print("HALO ");
+        }
+        System.out.println("");
+    }
+
+}
