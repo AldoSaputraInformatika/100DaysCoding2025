4c4179a571e3809e04211dffaa8d2d4a70fc5f60
Aldo Saputra / Informatika <aldopadaungan@gmail.com>
2025-11-21 18:13:30 +0800

    Day75.java

---
+++ Day75.java
@@ -0,0 +1,14 @@
+
+public class Day75 {
+
+    public static void main(String[] args) {
+
+        String kalimat = "Akulturasi Budaya Eropa dan Budaya Indonesia Terhadap Mie Ayam ";
+        //Total kata yang ada dalam string
+        System.out.println(kalimat.length());
+        //Membuat semua unsur teks jadi kapital
+        System.out.println(kalimat.toUpperCase());
+        //Membuat semua unsur teks jadi kecil
+        System.out.println(kalimat.toLowerCase());
+    }
+}
