18383349431dfebc2aaf8199fdca871b7b49ca5f
Aldo Saputra / Informatika <aldopadaungan@gmail.com>
2025-11-26 21:00:39 +0800

    Day80.java

---
+++ Day80.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Day80 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // 1. Array 1D tanpa inisialisasi langsung
+        String[] array = new String[5];
+        // 2. Array 1D dengan nisialisasi langsung
+        String[] array2 = {"A", "B", "C", "D", "E"};
+        
+        
+
+    }
+
+}
