7305536154a4026ac7d5446825932d85c589527e
Aldo Saputra / Informatika <aldopadaungan@gmail.com>
2025-11-27 21:52:26 +0800

    Update Day81.java

--- Day81.java
+++ Day81.java
@@ -1,14 +1,16 @@
 public class Day81 {
 
     public static void main(String[] args) {
-
         int[] array = new int[5];
         array[0] = 1;
         array[1] = 2;
         array[2] = 3;
         array[3] = 4;
         array[4] = 5;
+        System.out.println(array[0]);
+        System.out.println(array[2]);
 
     }
 
 }
+
