e6b8e015c8d3a6cda73731deac323a710a5938d2
Aldo Saputra / Informatika <aldopadaungan@gmail.com>
2025-12-13 21:46:24 +0800

    Day97.java

---
+++ Day97.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class Day97 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+    }
+    static double luaspersegi(double s){
+        double luas = s * s;
+        System.out.println(luas);
+        return luas;
+    }
+}
