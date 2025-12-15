53d5b65ca35ba2dc2cefe396ea604f9067060bd1
Aldo Saputra / Informatika <aldopadaungan@gmail.com>
2025-12-15 17:17:00 +0800

    Day99.java

---
+++ Day99.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class Day99 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        for (int i = 2; i <= n; i++) {
+            boolean prima = true;
+            for (int j = 2; j < i; j++) {
+                if(i % j == 0){
+                    prima = false;
+                    break;
+                }
+            }
+            if(prima){
+                System.out.println(i);
+            }
+        }
+
+    }
+
+}
